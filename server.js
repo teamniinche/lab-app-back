184d52d3f404f1e2b64d398ed283013c46612011
Teamniintche <142489947+teamniinche@users.noreply.github.com>
2026-09-12 10:55:04 +0200

    Refactor server.js and remove commented code

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ var PORT=process.env.PORT || 8081;
 // Intancier express
 var server=express();
 var sServer=http.createServer(server);
-const allowedOrigins = ["http://localhost:8080","http://172.17.224.1:8080"];  // Votre propre port API si le client y est hébergé;
+const allowedOrigins = ["http://localhost","http://172.17.224.1","http://localhost:8080","http://172.17.224.1:8080"];  // Votre propre port API si le client y est hébergé;
 const corsOptions={
     // origin: '*', // Permet toutes les origines, mais tu peux restreindre à une origine spécifique
     origin:function (origin, callback) {
